907013be478c81221534de8cf9ed03b41d51957f
huyIT203 <101252285+huyIT203@users.noreply.github.com>
2024-07-09 01:36:45 +0700

    Initial commit

---
+++ src/test/java/com/shop/clothing/ClothingApplication.java
@@ -0,0 +1,13 @@
+package com.shop.clothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
